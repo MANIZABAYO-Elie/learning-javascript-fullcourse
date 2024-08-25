48a16d86e35d6fe0316f53aed3b47965a75dcd46
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-25 17:01:22 +0200

    first commit

---
+++ elie.js
@@ -0,0 +1,29 @@
+//learning javascript full course
+
+
+
+console.log("hello world")
+//  variables declaration
+// variable is declared using assignment operator =
+let name = "Elie";
+console.log(name);
+// naming  variables
+//variables are named using camelCase
+// you can use digits, letters, dollar sign $
+// or  underscore _
+// must start with letter or dollar sign or underscore
+// no keyword
+// can not start with number
+let fullName = "MANIZABAYO ELIE"
+console.log(fullName);
+// LET VS CONST VS VAR
+//var is accessed in  and updated in scope
+var number = 25; 
+// const  can not be re-assigned
+const pi = 3.14;
+
+// let can be re re-assigned and is accessed in inside a function.
+
+
+
+
