c72178e79ca561b4173fd83332a0c357cdb48650
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-17 10:38:48 +0200

    file committed

--- codewars.js
+++ codewars.js
@@ -1,7 +1,8 @@
-// function reverseWords(str){
-//     const arrayWords = str.split(' ');
-//     const reversedArray = arrayWords.reverse(' ');
-//     return reversedArray.join(' ');
-// }
-// console.log(reverseWords("hello world"));
-
+ function reverseWords(str){
+        //   const arrayWords = str.split(' ');
+        //    const reversedArray = arrayWords.reverse(' ');
+        //    return reversedArray.join(' ');
+        const reversedwords = str.split(' ').reverse(' ').join(' ');
+        console.log(reversedwords);
+}
+console.log(reverseWords("Hello World"));
